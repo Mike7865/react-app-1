c62bb54a103b1a28db785c2f50a541f85b97b1b2
Mike38 <mshar50055@gmail.com>
2020-08-26 22:47:41 +0300

    components created

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className="header">
+    <img src="https://is4-ssl.mzstatic.com/image/thumb/Purple113/v4/ee/91/eb/ee91ebc6-f7e6-2fa2-356e-d5930900691b/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.png" />
+  </header>
+}
+
+export default Header;

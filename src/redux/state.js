111352bb3ed6754136d1b299dedab336f5db9c8b
Mike38 <mshar50055@gmail.com>
2020-09-15 21:40:48 +0300

    Dialogs.jsx modified - messenger added

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,5 +1,7 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+const UPDATE_NEW_MESSAGE_BODY = "UPDATE_NEW_MESSAGE_BODY";
+const SEND_MESSAGE= "SEND_MESSAGE";
 
 let store = {
   _state: {
@@ -25,7 +27,8 @@ let store = {
         {id: "3", message: "What are you doing?"},
         {id: "4", message: "Hey"},
         {id: "5", message: "Yo"}
-      ]
+      ],
+      newMessageBody: " "
     }
   },
   _callSubscriber() {
@@ -55,14 +58,27 @@ let store = {
 
       this._state.profilePage.newPostText = action.newText;
       this._callSubscriber(this._state);
+    } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+
+      this._state.dialogsPage.newMessageBody = action.body;
+      this._callSubscriber(this._state);
+    } else if (action.type === SEND_MESSAGE) {
+
+      let body = this._state.dialogsPage.newMessageBody;
+      this._state.dialogsPage.newMessageBody = " ";
+      this._state.dialogsPage.messages.push({id: 6, message: body});
+      this._callSubscriber(this._state);
     }
   }
 }
 
 export const addPostActionCreator = () => ({type: ADD_POST})
-
 export const updateNewPostTextActionCreator = (text) => 
-  ({type: UPDATE_NEW_POST_TEXT, newText: text})
+({type: UPDATE_NEW_POST_TEXT, newText: text})
+
+export const sendMessageCreator = () => ({type: SEND_MESSAGE})
+export const updateNewMessageBodyCreator = (body) => 
+({type: UPDATE_NEW_MESSAGE_BODY, body: body})
 
 export default store;
 
